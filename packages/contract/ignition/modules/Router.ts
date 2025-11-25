5d751689c15c75ac4cd0132142ed26d54de2fcdb
aowheel <aoimakino2003@gmail.com>
2025-11-25 10:14:44 +0900

    add biome and settings

--- packages/contract/ignition/modules/Router.ts
+++ packages/contract/ignition/modules/Router.ts
@@ -2,14 +2,14 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import RouterFactory from "./RouterFactory.js";
 
 export default buildModule("RouterModule", (m) => {
-	const { routerFactory } = m.useModule(RouterFactory);
+  const { routerFactory } = m.useModule(RouterFactory);
 
-	m.call(routerFactory, "deploy", [
-		m.getParameter("salt"),
-		m.getParameter("fundWallet"),
-		m.getParameter("fundRatio"),
-		m.getParameter("burnRatio"),
-	]);
+  m.call(routerFactory, "deploy", [
+    m.getParameter("salt"),
+    m.getParameter("fundWallet"),
+    m.getParameter("fundRatio"),
+    m.getParameter("burnRatio"),
+  ]);
 
-	return { routerFactory };
+  return { routerFactory };
 });
