e0477827ec366b53cff8ff1499560914cd5808db
yawn <69970183+yawn-c111@users.noreply.github.com>
2025-12-16 18:09:11 +0900

    feat: Implement allow list management and access control in FoRToken contract

--- packages/contract/contracts/FORToken.sol
+++ packages/contract/contracts/FORToken.sol
@@ -3,13 +3,137 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract FoRToken is ERC20, ERC20Permit {
+contract FoRToken is ERC20, ERC20Permit, AccessControl {
+    using ECDSA for bytes32;
+
+    // ============ Constants ============
+    /// @notice Admin role for managing allow list
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    /// @notice EIP712 typehash for AddToAllowList
+    bytes32 public constant ADD_TO_ALLOWLIST_TYPEHASH =
+        keccak256("AddToAllowList(address account,uint256 deadline)");
+
+    // ============ State Variables ============
+    mapping(address => bool) private _allowList;
+
+    // ============ Events ============
+    event AllowListAdded(address indexed account);
+    event AllowListRemoved(address indexed account);
+
+    // ============ Errors ============
+    error NotAllowListed(address account);
+    error InvalidSignature();
+    error SignatureExpired();
+    error AlreadyAllowListed();
+
+    // ============ Constructor ============
     constructor(
         uint256 initialSupply,
         string memory name,
         string memory symbol
     ) ERC20(name, symbol) ERC20Permit(name) {
-        _mint(msg.sender, initialSupply);
+        _mint(_msgSender(), initialSupply);
+        
+        // Grant roles to deployer
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(ADMIN_ROLE, _msgSender());
+        
+        // Add deployer to allow list
+        _allowList[_msgSender()] = true;
+        emit AllowListAdded(_msgSender());
+    }
+
+    // ============ AllowList Management ============
+
+    /// @notice Add an address to the allow list (ADMIN_ROLE only)
+    /// @param account Address to add
+    function addToAllowList(address account) external onlyRole(ADMIN_ROLE) {
+        if (_allowList[account]) revert AlreadyAllowListed();
+        _allowList[account] = true;
+        emit AllowListAdded(account);
+    }
+
+    /// @notice Remove an address from the allow list (ADMIN_ROLE only)
+    /// @param account Address to remove
+    function removeFromAllowList(address account) external onlyRole(ADMIN_ROLE) {
+        if (!_allowList[account]) revert NotAllowListed(account);
+        _allowList[account] = false;
+        emit AllowListRemoved(account);
+    }
+
+    /// @notice Add an address to the allow list using EIP712 signature from an admin
+    /// @param account Address to add
+    /// @param deadline Signature expiration timestamp
+    /// @param signature Admin's EIP712 signature
+    function addToAllowListWithSignature(
+        address account,
+        uint256 deadline,
+        bytes calldata signature
+    ) external {
+        if (block.timestamp > deadline) revert SignatureExpired();
+        if (_allowList[account]) revert AlreadyAllowListed();
+
+        bytes32 structHash = keccak256(
+            abi.encode(ADD_TO_ALLOWLIST_TYPEHASH, account, deadline)
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        address signer = hash.recover(signature);
+
+        if (!hasRole(ADMIN_ROLE, signer)) revert InvalidSignature();
+
+        _allowList[account] = true;
+        emit AllowListAdded(account);
+    }
+
+    /// @notice Check if an address is on the allow list
+    /// @param account Address to check
+    /// @return bool True if the address is on the allow list
+    function isAllowListed(address account) public view returns (bool) {
+        return _allowList[account];
+    }
+
+    // ============ Transfer Restrictions ============
+
+    /// @notice Override transfer to enforce allow list
+    function transfer(
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        _requireBothAllowListed(_msgSender(), to);
+        return super.transfer(to, amount);
+    }
+
+    /// @notice Override transferFrom to enforce allow list
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        _requireBothAllowListed(from, to);
+        return super.transferFrom(from, to, amount);
+    }
+
+    // ============ Internal Functions ============
+
+    /// @notice Require both addresses to be on the allow list
+    /// @param from Sender address
+    /// @param to Recipient address
+    function _requireBothAllowListed(address from, address to) internal view {
+        if (!_allowList[from]) revert NotAllowListed(from);
+        if (!_allowList[to]) revert NotAllowListed(to);
+    }
+
+    // ============ ERC165 Interface Support ============
+
+    /// @notice Override supportsInterface to support AccessControl
+    /// @param interfaceId Interface identifier to check
+    /// @return bool True if the interface is supported
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
--- packages/contract/contracts/FORToken.t.sol
+++ packages/contract/contracts/FORToken.t.sol
@@ -2,22 +2,41 @@
 pragma solidity ^0.8.28;
 
 import {Test} from "forge-std/Test.sol";
-import {FoRToken} from "./FoRToken.sol";
+import {FoRToken} from "./FORToken.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
 
 contract FoRTokenTest is Test {
-    FoRToken token;
-
-    address deployer = address(this);
-    address user1 = address(0x1);
-    address user2 = address(0x2);
-
+    // ============ Constants ============
     uint256 constant INITIAL_SUPPLY = 1_000_000 ether;
     string constant NAME = "FoR Token";
     string constant SYMBOL = "FOR";
 
+    uint256 constant ADMIN_PRIVATE_KEY = 0xA11CE;
+    bytes32 constant DEFAULT_ADMIN_ROLE = 0x00;
+
+    // ============ State Variables ============
+    FoRToken token;
+
+    // Test addresses
+    address deployer = address(this);
+    address user1 = address(0x1);
+    address user2 = address(0x2);
+    address adminSigner;
+
+    // Computed in setUp
+    bytes32 adminRole;
+
     function setUp() public {
         token = new FoRToken(INITIAL_SUPPLY, NAME, SYMBOL);
+        adminSigner = vm.addr(ADMIN_PRIVATE_KEY);
+        adminRole = token.ADMIN_ROLE();
+        
+        // Add user1 and user2 to allow list for basic transfer tests
+        token.addToAllowList(user1);
+        token.addToAllowList(user2);
     }
+
+    // ============ Basic ERC20 Tests ============
 
     function test_InitialMetadata() public view {
         require(
@@ -102,6 +121,296 @@ contract FoRTokenTest is Test {
         require(
             token.balanceOf(deployer) == INITIAL_SUPPLY - amount,
             "sender balance"
+        );
+    }
+
+    // ============ Initial State Tests ============
+
+    function test_DeployerHasDefaultAdminRole() public view {
+        require(token.hasRole(DEFAULT_ADMIN_ROLE, deployer), "deployer should have DEFAULT_ADMIN_ROLE");
+    }
+
+    function test_DeployerHasAdminRole() public view {
+        require(token.hasRole(adminRole, deployer), "deployer should have ADMIN_ROLE");
+    }
+
+    function test_DeployerIsAutomaticallyAllowListed() public view {
+        require(token.isAllowListed(deployer), "deployer should be allow listed");
+    }
+
+    // ============ AllowList Management Tests ============
+
+    function test_AddToAllowList() public {
+        address newUser = address(0x100);
+        token.addToAllowList(newUser);
+        require(token.isAllowListed(newUser), "user should be allow listed");
+    }
+
+    function test_AddToAllowList_EmitsEvent() public {
+        address newUser = address(0x100);
+        vm.expectEmit(address(token));
+        emit FoRToken.AllowListAdded(newUser);
+        token.addToAllowList(newUser);
+    }
+
+    function test_AddToAllowList_RevertsIfAlreadyAllowListed() public {
+        vm.expectRevert(FoRToken.AlreadyAllowListed.selector);
+        token.addToAllowList(user1); // user1 was added in setUp
+    }
+
+    function test_AddToAllowList_RevertsIfNotAdmin() public {
+        address newUser = address(0x100);
+        vm.prank(user1);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector,
+                user1,
+                adminRole
+            )
+        );
+        token.addToAllowList(newUser);
+    }
+
+    function test_AddToAllowList_AdminRoleCanAdd() public {
+        token.grantRole(adminRole, adminSigner);
+        address newUser = address(0x100);
+        vm.prank(adminSigner);
+        token.addToAllowList(newUser);
+        require(token.isAllowListed(newUser), "user should be allow listed");
+    }
+
+    function test_RemoveFromAllowList() public {
+        token.removeFromAllowList(user1);
+        require(!token.isAllowListed(user1), "user1 should not be allow listed");
+    }
+
+    function test_RemoveFromAllowList_EmitsEvent() public {
+        vm.expectEmit(address(token));
+        emit FoRToken.AllowListRemoved(user1);
+        token.removeFromAllowList(user1);
+    }
+
+    function test_RemoveFromAllowList_RevertsIfNotInAllowList() public {
+        address newUser = address(0x100);
+        vm.expectRevert(abi.encodeWithSelector(FoRToken.NotAllowListed.selector, newUser));
+        token.removeFromAllowList(newUser);
+    }
+
+    function test_RemoveFromAllowList_RevertsIfNotAdmin() public {
+        vm.prank(user2);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector,
+                user2,
+                adminRole
+            )
+        );
+        token.removeFromAllowList(user1);
+    }
+
+    // ============ EIP712 Signature Tests ============
+
+    function test_AddToAllowListWithSignature() public {
+        // Add adminSigner as admin
+        token.grantRole(adminRole, adminSigner);
+        
+        address newUser = address(0x100);
+        uint256 deadline = block.timestamp + 1 hours;
+        
+        bytes32 structHash = keccak256(
+            abi.encode(token.ADD_TO_ALLOWLIST_TYPEHASH(), newUser, deadline)
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ADMIN_PRIVATE_KEY, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        
+        token.addToAllowListWithSignature(newUser, deadline, signature);
+        
+        require(token.isAllowListed(newUser), "user should be allow listed");
+    }
+
+    function test_AddToAllowListWithSignature_EmitsEvent() public {
+        token.grantRole(adminRole, adminSigner);
+        
+        address newUser = address(0x100);
+        uint256 deadline = block.timestamp + 1 hours;
+        
+        bytes32 structHash = keccak256(
+            abi.encode(token.ADD_TO_ALLOWLIST_TYPEHASH(), newUser, deadline)
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ADMIN_PRIVATE_KEY, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        
+        vm.expectEmit(address(token));
+        emit FoRToken.AllowListAdded(newUser);
+        token.addToAllowListWithSignature(newUser, deadline, signature);
+    }
+
+    function test_AddToAllowListWithSignature_RevertsIfExpired() public {
+        token.grantRole(adminRole, adminSigner);
+        
+        address newUser = address(0x100);
+        uint256 deadline = block.timestamp - 1; // Already expired
+        
+        bytes32 structHash = keccak256(
+            abi.encode(token.ADD_TO_ALLOWLIST_TYPEHASH(), newUser, deadline)
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ADMIN_PRIVATE_KEY, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        
+        vm.expectRevert(FoRToken.SignatureExpired.selector);
+        token.addToAllowListWithSignature(newUser, deadline, signature);
+    }
+
+    function test_AddToAllowListWithSignature_RevertsIfAlreadyAllowListed() public {
+        token.grantRole(adminRole, adminSigner);
+        
+        uint256 deadline = block.timestamp + 1 hours;
+        
+        bytes32 structHash = keccak256(
+            abi.encode(token.ADD_TO_ALLOWLIST_TYPEHASH(), user1, deadline)
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ADMIN_PRIVATE_KEY, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        
+        vm.expectRevert(FoRToken.AlreadyAllowListed.selector);
+        token.addToAllowListWithSignature(user1, deadline, signature);
+    }
+
+    function test_AddToAllowListWithSignature_RevertsIfSignerNotAdmin() public {
+        // adminSigner is NOT added as admin
+        address newUser = address(0x100);
+        uint256 deadline = block.timestamp + 1 hours;
+        
+        bytes32 structHash = keccak256(
+            abi.encode(token.ADD_TO_ALLOWLIST_TYPEHASH(), newUser, deadline)
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ADMIN_PRIVATE_KEY, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        
+        vm.expectRevert(FoRToken.InvalidSignature.selector);
+        token.addToAllowListWithSignature(newUser, deadline, signature);
+    }
+
+    function test_AddToAllowListWithSignature_RevertsIfInvalidSignature() public {
+        token.grantRole(adminRole, adminSigner);
+        
+        address newUser = address(0x100);
+        uint256 deadline = block.timestamp + 1 hours;
+        
+        // Sign with wrong account data
+        bytes32 structHash = keccak256(
+            abi.encode(token.ADD_TO_ALLOWLIST_TYPEHASH(), address(0x999), deadline)
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ADMIN_PRIVATE_KEY, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        
+        // Try to use signature for different account
+        vm.expectRevert(FoRToken.InvalidSignature.selector);
+        token.addToAllowListWithSignature(newUser, deadline, signature);
+    }
+
+    function test_AddToAllowListWithSignature_AnyoneCanCall() public {
+        token.grantRole(adminRole, adminSigner);
+        
+        address newUser = address(0x100);
+        uint256 deadline = block.timestamp + 1 hours;
+        
+        bytes32 structHash = keccak256(
+            abi.encode(token.ADD_TO_ALLOWLIST_TYPEHASH(), newUser, deadline)
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ADMIN_PRIVATE_KEY, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        
+        // Anyone (user2) can call the function
+        vm.prank(user2);
+        token.addToAllowListWithSignature(newUser, deadline, signature);
+        
+        require(token.isAllowListed(newUser), "user should be allow listed");
+    }
+
+    // ============ Transfer Restriction Tests ============
+
+    function test_Transfer_RevertsIfSenderNotAllowListed() public {
+        address notAllowListed = address(0x100);
+        
+        // Give some tokens to notAllowListed
+        token.addToAllowList(notAllowListed);
+        token.transfer(notAllowListed, 100 ether);
+        token.removeFromAllowList(notAllowListed);
+        
+        vm.prank(notAllowListed);
+        vm.expectRevert(abi.encodeWithSelector(FoRToken.NotAllowListed.selector, notAllowListed));
+        token.transfer(user1, 50 ether);
+    }
+
+    function test_Transfer_RevertsIfRecipientNotAllowListed() public {
+        address notAllowListed = address(0x100);
+        
+        vm.expectRevert(abi.encodeWithSelector(FoRToken.NotAllowListed.selector, notAllowListed));
+        token.transfer(notAllowListed, 100 ether);
+    }
+
+    function test_Transfer_SucceedsIfBothAllowListed() public {
+        uint256 amount = 100 ether;
+        
+        token.transfer(user1, amount);
+        
+        require(token.balanceOf(user1) == amount, "transfer should succeed");
+    }
+
+    function test_TransferFrom_RevertsIfFromNotAllowListed() public {
+        address notAllowListed = address(0x100);
+        
+        // Give some tokens and approval
+        token.addToAllowList(notAllowListed);
+        token.transfer(notAllowListed, 100 ether);
+        token.removeFromAllowList(notAllowListed);
+        
+        vm.prank(notAllowListed);
+        token.approve(user1, 100 ether);
+        
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(FoRToken.NotAllowListed.selector, notAllowListed));
+        token.transferFrom(notAllowListed, user2, 50 ether);
+    }
+
+    function test_TransferFrom_RevertsIfToNotAllowListed() public {
+        address notAllowListed = address(0x100);
+        
+        token.approve(user1, 100 ether);
+        
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(FoRToken.NotAllowListed.selector, notAllowListed));
+        token.transferFrom(deployer, notAllowListed, 50 ether);
+    }
+
+    function test_TransferFrom_SucceedsIfBothAllowListed() public {
+        uint256 amount = 100 ether;
+        
+        token.approve(user1, amount);
+        
+        vm.prank(user1);
+        token.transferFrom(deployer, user2, amount);
+        
+        require(token.balanceOf(user2) == amount, "transfer should succeed");
+    }
+
+    // ============ Helper Functions ============
+
+    function _hashTypedDataV4(bytes32 structHash) internal view returns (bytes32) {
+        return keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                token.DOMAIN_SEPARATOR(),
+                structHash
+            )
         );
     }
 }
